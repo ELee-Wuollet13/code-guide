1ad4b3402259c77d3429e88786cfedf67631e99e
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-09-20 20:28:09 -0700

    set up javascript

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,28 @@ $(document).ready(function()) {
      result += parseInt($("input:radio[name=question1]:checked").val());
       result += parseInt($("input:radio[name=question1]:checked").val());
 
-      
-  })
-}
+      if ($("input:radio[name=logic]:checked").val() === "simple")
+      if (result >= 3) {
+        $(".GO").show();
+      }
+      else if (result < 3 && result > -3) {
+        $(".Ruby").show();
+      }
+      else {
+          $(".Javascript").show();
+      }
+    }
+    else if ($("input:radio[name=logic]:checked").val() === "complex") {
+      if (result >= 3).show();
+    }
+    else if (result < 3 && result > -3) {
+      $(".c++").show();
+    }
+    else {
+      $(".python").show();
+    }
+    else {
+      $(".nothing").show();
+    }
+  });
+});

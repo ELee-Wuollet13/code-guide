91e2924ea3dc2d0ddf04063577c606a3e1f79ccb
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-09-20 19:59:20 -0700

    further html and connect jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function()) {
   $("#questions").submit(function(event) {
     event.preventDefault();
-    
+    $("#lingo").show();
+    var result = parseInt($("input:radio[name=question1]:checked").val());
+     result += parseInt($("input:radio[name=question1]:checked").val());
+      result += parseInt($("input:radio[name=question1]:checked").val());
+
+      
   })
 }

722605e565a4afa8001ffb8e63d24dc145dfcde4
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-09-23 14:37:55 -0700

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $("#questions").submit(function(event) {
     $("#answers").show();
-    // $(".hidden").hide();
-
     var result = parseInt($("input:radio[name=question1]:checked").val());
     result += parseInt($("input:radio[name=question2]:checked").val());
     result += parseInt($("input:radio[name=question3]:checked").val());
@@ -11,27 +9,22 @@ $(document).ready(function() {
 
     if (result >= 5) {
       $(".GO").show();
-      console.log(result)
     }
 
     else if (result < 5 && result > 2) {
       $(".Ruby").show();
-      console.log(result)
     }
 
     else if (result <= 2 && result >= -2){
       $(".Javascript").show();
-      console.log(result)
     }
 
     else if (result <-2 && result > -5) {
       $(".csharp").show();
-      console.log(result)
     }
 
     else {
       $(".Python").show();
-      console.log(result)
     }
     event.preventDefault();
   });

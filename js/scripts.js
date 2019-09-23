e1463c36fc2db84ca51c35e3dc1ed18a71f31ca4
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-09-23 12:40:55 -0700

    finish functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
   $("#questions").submit(function(event) {
-    $("#lingo").show();
+    $("#answers").show();
+    // $(".hidden").hide();
+
     var result = parseInt($("input:radio[name=question1]:checked").val());
     result += parseInt($("input:radio[name=question2]:checked").val());
     result += parseInt($("input:radio[name=question3]:checked").val());
     result += parseInt($("input:radio[name=question4]:checked").val());
-          console.log(result)
+    console.log(result)
 
     if (result >= 5) {
       $(".GO").show();
-            console.log(result)
+      console.log(result)
     }
 
     else if (result < 5 && result > 2) {
       $(".Ruby").show();
-            console.log()
+      console.log(result)
     }
 
     else if (result <= 2 && result >= -2){
       $(".Javascript").show();
-            console.log(result)
+      console.log(result)
     }
 
     else if (result <-2 && result > -5) {
-      $(".c++").show();
-            console.log(result)
+      $(".csharp").show();
+      console.log(result)
     }
 
     else {
-      $(".python").show();
+      $(".Python").show();
       console.log(result)
     }
     event.preventDefault();
